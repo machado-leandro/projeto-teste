7e527b45598d0901a7beb5157ae47892412fa14d
machado-leandro <69988365+machado-leandro@users.noreply.github.com>
2020-08-21 15:55:57 -0300

    Add files via upload

--- projeto.php
+++ projeto.php
@@ -18,47 +18,186 @@ tetosolar (s/n), cambio aut(s/n), vol_combustivel
 */
 //1.implemente uma classe proprietario
 class Proprietario{
+	function GetProprietario(){
+	return $this->$proprietario;
+}
+	function setProprietario($Proprietario){
+	
+}
+
 //2. declare os segintes atributos
 	private $nome;
+	public function getNome(){
+		return $this->nome;
+	}
+	public function setnome($nome){
+		$this->nome=$nome;
+	}
+	
 	private $CPF;
+	public function getCPF(){
+		return $this->cpf;
+	}
+	public function setCPF($CPF){
+		$this->CPF = $CPF;
+	}
+
 	private $RG;
+	public function getRG(){
+		return $this->RG;
+	}
+	public function setRG($RG){
+		$this->RG = $RG;
+	}
+
 	private $dt_Nasc;
+	public function getdt_nasc(){
+		return $this->dt_Nasc;
+	}
+	public function setdt_nasc($dt_Nasc){
+		$this->dt_Nasc=$dt_Nasc;
+	}
+
 	private $rua;
+	public function getRua(){
+		return $this->rua;
+	}
+	public function setRua($rua){
+		$this->rua=$rua;
+	}
+
 	private $bairro;
+	public function getbairro(){
+		return $this->bairro;
+	}
+	public function setbairro($bairro){
+		$this->bairro=$bairro;
+	}
+
 	private $estado;
+	public function getestado(){
+		return $this->estado;
+	}
+	public function setestado($estado){
+		$this->Estado=$estado;
+	}
+
 	private $CEP;
+	public function getCEP(){
+		return $this->CEP;
+	}
+	public function setCEP($CEP){
+		$this->CEP=$CEP;
+	}
+
 	private $complemento;
-	private function __construct($nome, $CPF, $RG){
+	public function __construct($nome, $CPF, $RG){
 		$this-> nome = $nome;
 		$this-> cpf = $CPF;
 		$this-> rg = $RG;
 	}
+
+
 }
 
 class carro{
 	private $modelo;
+	public function getmodelo(){
+		return $this->modelo;
+	}
+	public function setmodelo($modelo){
+		$this->modelo=$modelo;
+	}
+
 	private $cor;
+	public function getCor(){
+		return $this->cor;
+	}
+	public function setCor($cor){
+		$this->cor=$cor;
+	}
+
 	private $ano;
-	private $marca;
-	private $chassi;
-	private $proprietario;
-	private $vel_max;
-	private $vel_atual;
-	private $nr_portas;
-	private $tetosolar;
-	private $cambio_aut;
-	private $vol_combustivel;
-}
-
-//3. encapsulamento
-function setProprietario($Proprietario){
+	public function getAno(){
+		return $this->ano;
+	}
+	public function setAno($ano){
+		$this->ano=$ano;
+	}
 	
-}
+	private $marca;
+	public function getMarca(){
+		return $this->marca;
+	}
+	public function setMarca($marca){
+		$this->marca=$marca;
+	}
 
-function GetProprietario(){
-	echo"proprietario é: {$this->Proprietario}";
-}
+	private $chassi;
+	public function getChassi(){
+		return $this->chassi;
+	}
+	public function setChassi($chassi){
+		$this->Chassi=$chassi;
+	}
 
+	private $proprietario;
+	public function getProprietario(){
+		return $this->proprietario;
+	}
+	public function setProprietario($proprietario){
+		$this->proprietario=$proprietario;
+	}
+
+	private $vel_max;
+	public function getVel_max(){
+		return $this->vel_max;
+	}
+	public function setVel_max($vel_max){
+		$this->vel_max=$vel_max;
+	}
+
+	private $vel_atual;
+	public function getVel_atual(){
+		return $this->vel_atual;
+	}
+	public function setVel_atual($vel_atual){
+		$this->vel_atual=$vel_atual;
+	}
+
+	private $nr_portas;
+	public function setNr_portas(){
+		return $this->nr_portas;
+	}
+	public function getNr_portas($nr_portas){
+		$this->nr_portas=$nr_portas;
+	}
+
+	private $tetosolar;
+	public function getTetosolar(){
+		return $this->tetosolar;
+	}
+	public function setTetosolr($tetosolar) {
+		$this->tetosolar=$tetosolar;
+	}
+
+	private $cambio_aut;
+	public function getCambio_aut(){
+		return $this->$cambio_aut;
+	}
+	public function setCambio_aut($cambio_aut){
+		$this->cambio_aut=$cambio_aut;
+	}
+
+	private $vol_combustivel;
+	public function getvol_Combustivel(){
+		return $this->vol_combustivel;
+	}
+	public function setvol_Combustivel($vol_combustivel){
+		$this->vol_combustivel=$vol_combustivel;
+	}
+
+}
 ?>
 
 
